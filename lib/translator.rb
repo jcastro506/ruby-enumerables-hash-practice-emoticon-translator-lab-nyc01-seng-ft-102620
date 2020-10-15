be8a1d5676a9ae5f6a9ec4a64ff1a67897635a44
Josh Castro <castrojoshua805@gmail.com>
2020-10-15 19:26:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,7 @@ def get_japanese_emoticon(data, emoticon)
   emoticons = load_library(data)
   emoticons.each do |key, value|
     if emoticons[key][:english] == emoticon 
+      binding.pry 
       return key 
   end 
   #binding.pry 
